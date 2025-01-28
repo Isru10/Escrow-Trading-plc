777265c9db1ec9de05746d637cedcc59884c986a
Israel Sol <isrucrasus10@gmail.com>
2025-01-28 21:07:56 +0300

    added framer and react and tranquil app all in one

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Services from './components/Services'
+import Analytics from './components/Analytics'
+import Newsletter from './components/Newsletter'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
 function App() {
   return (
-    <div className="flex flex-col gap-8">
-      <Navbar/>
-      <Hero/> 
-      <Services/>
-    </div>
+          <div className="">
+            <Navbar />
+            <Hero />
+            <Analytics/>
+            <Newsletter/>
+            <Cards/>
+            <Footer/>
+          </div>
+
+
   )
 }
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,107 +1,36 @@
-import { FaBars, FaHeart, FaShoppingCart, FaTimes, FaUser } from 'react-icons/fa'
-import Escrow from '../assets/Escrow.png'
-import { useEffect, useState } from 'react'
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
 const Navbar = () => {
-  const navLinks=[
-    {title:'Home',url:'/'},
-    {title:'About',url:'/about'},
-    {title:'Services',url:'/servcies'},
-    {title:'Contact',url:'/contact'},
-  ]
-  const iconList = [
-    {icon:<FaUser/>},
-    {icon:<FaHeart/>},
-    {icon:<FaShoppingCart/>},
-  ]
-  const bgColor='bg-gray-800';
-  const modalColor='bg-gray-900'
+  const [nav, setNav] = useState(false);
 
-
-  // st-2
-  const [isMobile,setIsMobile]=useState(window.innerWidth<769);
-  const [showModal,setShowModal]=useState(false);
-  useEffect(()=>{
-    const handleResize=()=>{{
-      setIsMobile(window.innerWidth<769)
-    }}
-    window.addEventListener('resize',handleResize)
-    return ()=>{
-      window.removeEventListener('resize',handleResize)
-    };
-  },[])
-  const toggleModal =()=>{
-    setShowModal(!showModal);
+  const handleNav = () => {
+    setNav(!nav);
   };
-  const handleBarsIconClick=()=>{
-    toggleModal();
-  }
+
   return (
-    // <nav className='flex justify-between items-center bg-slate-600 '>
-    //     <div className="p-2">
-    //         <img src={Escrow} alt="" width={45} height={45} className='rounded hover:p-1 hover:rounded-full' />
-    //     </div>
-
-    //     <div className="gap-8 text-1xl p-1 flex justify-between  ">
-    //             <a href="/" className='hover:bg-slate-400 hover:outline-double hover:rounded-full hover:p-1' > Home</a>
-    //             <a href="/" className='hover:bg-slate-400 hover:outline-double hover:rounded-full hover:p-1'> About</a>
-    //             <a href="/" className='hover:bg-slate-400 hover:outline-double hover:rounded-full hover:p-1'> Services</a>
-    //             <a href="/" className='hover:bg-slate-400 hover:outline-double hover:rounded-full hover:p-1'> Contact</a>
-    //     </div>
-
-    // </nav>
-
-    <> 
-    {!isMobile ? (
-        <nav className={` ${bgColor}`}>
-          <div className="flex justify-between mx-auto items-center py-4 px-24">
-            <div className="text-white font-bold text-xl">Logo</div>
-            <ul className='flex gap-8 md:gap-16 items-center justify-center text-center cursor-pointer'>
-              {navLinks.map((link,index)=>(
-                <li key={index} className='text-white text-sm'>{link.title}</li>
-              ))}
-            </ul>
-
-            <ul className='flex text-white gap-6 items-center cursor-pointer'>
-              {iconList.map((item,index)=>(
-                <div key={index}>{item.icon}</div>
-              ))}
-            </ul>
-          </div>
-        </nav>
-      ):(
-        // nav for mobile 
-        <nav className={`${bgColor} py-4 px-4`}>
-          <div className="mx-auto flex items-center justify-between">
-            <div className="text-white font-bold text-xl">Logo</div>
-            <div className="flex justify-end items-center gap-6 text-white cursor-pointer">
-            {iconList.map((item,index)=>(
-                <div key={index} onClick={index===iconList.length-1?handleBarsIconClick:null}>{item.icon}</div>
-              ))}
-              <FaBars onClick={handleBarsIconClick} className='text-white cursor-pointer'/>
-            </div>
-          </div>
-
-          {showModal && (
-            <div className="fixed inset-0 flex justify-center items-center">
-              <div className={`absolute inset-0 ${modalColor}`}/>
-              <FaTimes className='absolute top-6 right-4 text-white cursor-pointer' onClick={toggleModal} style={{fontSize:'16px'}} />
-              <div className="relative bg-gray-900 w-full"> 
-                <div className="flex flex-col gap-8 items-center justify-center h-full">
-                {navLinks.map((link,index)=>(
-                <span key={index} className='text-white cursor-pointer font-light text-2xl'>{link.title}</span>
-              ))}
-                </div>
-              </div>
-            </div>
-          )}
-        </nav>
-      )
-    }
-    
-    </>
-
-    
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul className='hidden md:flex cursor-pointer'> 
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>Resources</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+      </div>
+      <ul className={nav ? 'cursor-pointer fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden' : 'ease-in-out duration-500 fixed left-[-100%] cursor-pointer'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Company</li>
+          <li className='p-4 border-b border-gray-600'>Resources</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4'>Contact</li>
+      </ul>
+    </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
